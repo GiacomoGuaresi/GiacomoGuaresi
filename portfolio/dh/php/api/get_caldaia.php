4dfe0d51e3d3cf1dae53050cd7a4656857630b58
Giacomo Guaresi <guaresi.giacomo@gmail.com>
2025-09-17 10:24:59 +0200

    feat: remove dh

--- portfolio/dh/php/api/get_caldaia.php
+++
@@ -1,8 +0,0 @@
-<?php 
-    include_once("../connection.php");
-    $sql = "SELECT * FROM  `currentVariable` where `Key` = 'Caldaia'";
-    $res = $mysqli->query($sql);
-    $row = $res->fetch_assoc();
-    $jsonObj[$row["Key"]] = $row["Value"]; 
-    echo json_encode($jsonObj);
-?>
